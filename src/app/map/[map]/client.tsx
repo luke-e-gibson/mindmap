9f030905fddab2fb12621d8cfb327be13c002805
luke-e-gibson <luke.e.gibson@outlook.com>
2024-12-22 21:36:35 +1100

    add header

--- src/app/map/[map]/client.tsx
+++ src/app/map/[map]/client.tsx
@@ -1,26 +1,46 @@
 "use client";
 import "@xyflow/react/dist/style.css";
 import "@/styles/mapCss.css";
-
+import { useRouter } from "next/navigation";
 import { Background, Controls, Panel, ReactFlow } from "@xyflow/react";
-import { Button } from "@/app/_components/components";
+import { Button } from "@/components/ui/button";
 
-export default function MapPageClient() {
+export default function MapPageClient({ page }: { page: string }) {
   return (
     <main className="h-full w-full">
       <ReactFlow>
+        <Header page={page} />
         <Background />
         <Controls showInteractive={false} />
-        <div className="p-4">
-          <div className="sticky z-10 flex w-full justify-between rounded border bg-white p-3">
-            <h1 className="items-center text-xl">Mind Map</h1>
-            <div className="flex justify-between gap-3 px-2">
-              <Button>New</Button>
-              <Button>Save</Button>
-            </div>
-          </div>
-        </div>
       </ReactFlow>
     </main>
+  );
+}
+
+function Header({page}: {page: string}) {
+  const router = useRouter();
+
+  function handelNew() {
+    if (confirm("Are you sure you want to create a new map?")) router.push("/map/new");
+  } 
+
+  function handelSave() {
+    if(page === "new") {
+      alert("Not implemented yet")
+    }else {
+      alert("Not implemented yet")
+    }
+  }
+
+  return (
+    <div className="p-4">
+      <div className="sticky z-10 flex w-full justify-between rounded-lg border bg-white p-3">
+        <h1 className="items-center text-xl">Mind Map</h1>
+        <div className="flex justify-between gap-3 px-2">
+          <Button variant="outline" onClick={()=>handelSave()}>Save</Button>
+          <Button variant="default" onClick={()=>handelNew()}>New</Button>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/app/map/[map]/page.tsx
+++ src/app/map/[map]/page.tsx
@@ -1,12 +1,11 @@
 import MapPageClient from "./client";
 
-export default async function Map({ params }: { params: Promise<{map: string}> }) {
-    const document = (await params).map
-    
-    if(document === "new") {
-        return <MapPageClient />
-    } else {
-        return <h1> Not implemented </h1>
-    }
+export default async function Map({
+  params,
+}: {
+  params: Promise<{ map: string }>;
+}) {
+  const document = (await params).map;
 
+  return <MapPageClient page={document} />;
 }
